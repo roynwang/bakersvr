64e32683d02b7adc905b3f753fac4b79cc174a44
roynwang <royn.wang.renyuan@gmail.com>
2015-10-22 16:44:21 +0800

    add support for register with username&password

--- baker/usr/urls.py
+++ baker/usr/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import patterns, url,include
 from usr.views import *
 
 urlpatterns = patterns('',
-		#url(r'^api/n/',register),
-		#url(r'^api/v/$','rest_framework_jwt.views.obtain_jwt_token'),
-		#url(r'^api/t/', 'rest_framework_jwt.views.refresh_jwt_token'),
+		url(r'^api/register/',register),
+		url(r'^api/login/$','rest_framework_jwt.views.obtain_jwt_token'),
+		url(r'^api/refreshtoken/', 'rest_framework_jwt.views.refresh_jwt_token'),
 		url(r'^api/user/$', UserList.as_view()),
         url(r'^api/user/(?P<name>[a-zA-Z0-9]{4,20})/$', UserItem.as_view()),
 		)
--- baker/usr/views.py
+++ baker/usr/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
 from .serializers import *
+from django.contrib.auth import get_user_model
+from rest_framework.response import Response
 from rest_framework import generics
+from rest_framework import status
+from rest_framework.decorators import api_view
+from django.shortcuts import render,get_object_or_404
+
 # Create your views here.
+@api_view(['POST'])
+def register(request):
+	from rest_framework_jwt.settings import api_settings
+	jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+	jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+
+	usr = None
+	auth_usr = None
+	if User.objects.filter(name=request.data["username"]).exists():
+		return Response({"err_no":101,"msg":"duplciate username"}, status=status.HTTP_400_BAD_REQUEST)
+	auth_usr = get_user_model().objects.create_user(username=request.data["username"],password=request.data["password"])
+	usr = User.objects.create(name=request.data["username"],
+			displayname=request.data["username"],
+			avatar="",
+			signature="",
+			sex = "",
+		)
+	#auth_usr = get_user_model().objects.get(username=number)
+	payload = jwt_payload_handler(auth_usr)
+	resp = {'token':jwt_encode_handler(payload)}
+	return Response(resp, status=status.HTTP_201_CREATED)
+
 
 class UserList(generics.CreateAPIView):
 	queryset = User.objects.all()
@@ -11,4 +39,6 @@ class UserItem(generics.RetrieveUpdateDestroyAPIView):
 	lookup_field = "name"
 	queryset = User.objects.all()
 	serializer_class = UserBasicSerializer
+
+
 
